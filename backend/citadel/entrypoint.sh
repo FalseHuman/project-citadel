b902a4181526126a70043806c62cd15a160226d0
FalseHuman <emilkhazioff@yandex.com>
2022-09-24 18:28:45 +0500

    norn certs

--- backend/citadel/entrypoint.sh
+++ backend/citadel/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-gunicorn --certfile=/etc/nginx/certs/ssl/crm.khazieff.crt --keyfile=/etc/nginx/certs/ssl/crm.khazieff.key core.wsgi:application --bind 0.0.0.0:443
+gunicorn  core.wsgi:application --bind 0.0.0.0:8000
