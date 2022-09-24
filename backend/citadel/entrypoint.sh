dc2372ae653730168f4e8352ddd5c2ec570e06ab
FalseHuman <emilkhazioff@yandex.com>
2022-09-24 18:22:46 +0500

    ssl path add

--- backend/citadel/entrypoint.sh
+++ backend/citadel/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
+gunicorn --certfile=/etc/nginx/certs/ssl/certs/crm.khazieff.crt --keyfile=/etc/ssl/certs/crm.khazieff.key core.wsgi:application --bind 0.0.0.0:443
