c5230d77d82f1b5d60c6f93cac2174af3370450f
Emil <emilkhazioff@yandex.com>
2022-12-29 12:11:51 +0500

    Update email_send.py

--- backend/citadel/tabs/management/commands/email_send.py
+++ backend/citadel/tabs/management/commands/email_send.py
@@ -1,8 +1,13 @@
 from django.core.management.base import BaseCommand
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from tabs.models import User, Pays
 from core.settings import DATE_FORMAT
 import datetime
+from django.conf import settings
+from django.http import HttpResponse
+from io import BytesIO
+from django.template.loader import render_to_string
+import weasyprint
 
 
 class Command(BaseCommand):
@@ -10,18 +15,50 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.stdout.write("Начат процесс рассылки...")
-        month = datetime.date.today().month
+        self.stdout.write(str(settings.STATIC_ROOT)+'/css/pdf.css')
+        data = datetime.date.today()
         # TODO: реализовать массовую рассылку
         try:
             email_send_user = User.objects.filter(email_send=True)
             for i in range(len(email_send_user)):
-                arr = Pays.objects.filter(author=email_send_user[i].id)
-
                 subject = 'Доброго времени суток {}, ваши расходы/доходы за этот месяц:'.format(
                     email_send_user[i].username)
+                message = "Выписка расходов/доходов за этот месяц"
+                email=EmailMessage(subject,message,
+                    'emilkhazioff@yandex.ru',
+                [email_send_user[i].email])
+                # Формирование PDF.
+                user = User.objects.get(id=email_send_user[i].id)
+                pays = Pays.objects.filter(author=email_send_user[i].id)
+                pays_cost = []
+                expense = []
+                income = []
+                total = 0
+                for z in range(len(pays)):
+                    if datetime.datetime.date(pays[z].data).month == data.month:
+                        pays_cost.append(pays[z])
+                for i in pays_cost:
+                    if i.cost > 0:
+                        income.append(i.cost)
+                    else:
+                        expense.append(i.cost)
+                    total += i.cost
+                html = render_to_string('orders/order/pdf.html', {
+                            'user': user, 'data': data, 'pays': pays_cost, 'total': total, "income": sum(income), 'expense': sum(expense)})
+                out = BytesIO()
+
+                stylesheets=[weasyprint.CSS(str(settings.STATIC_ROOT) + '/css/pdf.css')]
+                weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+                # Прикрепляем PDF к электронному сообщению.
+                email.attach('Exract_for_{}.pdf'.format(user.username), out.getvalue(),'application/pdf')
+                # Отправка сообщения.'''
+                email.send()
+                #arr = Pays.objects.filter(author=email_send_user[i].id)
+                '''subject = 'Доброго времени суток {}, ваши расходы/доходы за этот месяц:'.format(
+                    email_send_user[i].username)
                 message = ""
-                count = 0
-                for z in range(len(arr)):
+                count = 0'''
+                '''for z in range(len(arr)):
                     if datetime.datetime.date(arr[z].data).month == month:
                         count += 1
                         message += str(count) + '. ' + "Назначение платежа: " + str(arr[z].title) + "\n" + "    Описание платежа: " + str(arr[z].body) + "\n" + "    Сумма списания/пополнения: " + str(
@@ -31,7 +68,7 @@ class Command(BaseCommand):
                     message = "В этом месяце вы не вносили в систему расходы/доходы."
 
                 send_mail(subject, message, 'emilkhazioff@yandex.ru',
-                          [email_send_user[i].email])
+                          [email_send_user[i].email])'''
             self.stdout.write("Сообщения отправлены.")
         except User.DoesNotExist:
             pass
